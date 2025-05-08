b05149c69c571473ae11e1f2ae4997a3a59a075d
Apexxo-01 <mail@apexxo.com>
2025-05-08 12:31:46 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,29 +1,15 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AppLinkPage from "./pages/AppLinkPage";
-import { useEffect } from "react";
-
-function RootRedirect() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const storedSlug = localStorage.getItem("clientSlug");
-    if (storedSlug) {
-      navigate(`/app/${storedSlug}`);
-    } else {
-      navigate("/404");
-    }
-  }, [navigate]);
-  return <div>Loading app...</div>;
-}
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<RootRedirect />} />
+        <Route path="/" element={<div>Loading app...</div>} />
         <Route path="/app/:clientSlug" element={<AppLinkPage />} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
