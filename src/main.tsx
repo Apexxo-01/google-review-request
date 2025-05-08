69efbcccd29f7e8ca11922c6b11959e1d9868d25
Apexxo-01 <mail@apexxo.com>
2025-05-08 11:14:14 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
+import { registerServiceWorker } from './swRegistration';
+
+registerServiceWorker();
