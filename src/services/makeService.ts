79ee7e1af2a92d1c62a099387d056d48553a5675
Apexxo-01 <mail@apexxo.com>
2025-05-08 10:14:47 +0200

    Delete src directory

--- src/services/makeService.ts
+++
@@ -1,9 +0,0 @@
-
-export async function getClientLinkBySlug(slug: string) {
-  const url = `https://hook.us1.make.com/hlku66tksl5mr6z77lxwd1hvbdew77mt?slug=${slug}`;
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error("Failed to fetch client data");
-  }
-  return await res.json();
-}
